3d6e9ed010917a107da8ec7a2f5c3f93c221e224
Ericgacoki <gacokieric@gmail.com>
2021-05-01 22:36:51 +0300

    Application ViewModel
    Signed-off-by: Ericgacoki <gacokieric@gmail.com>

--- app/src/main/java/com/ericg/groom/data/UserViewModel.kt
+++ app/src/main/java/com/ericg/groom/data/UserViewModel.kt
@@ -6,20 +6,27 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class UserViewModel(application: Application) : AndroidViewModel(application) {
     private val readAllData: LiveData<List<User>>
     private val repository: UserRepository
-
     init {
         val userDao = UserDatabase.getDatabase(application).userDao()
         repository = UserRepository(userDao)
         readAllData = repository.readAllData
     }
 
-    fun addUser(user: User){
+    fun addUser(user: User) {
         viewModelScope.launch(Dispatchers.IO) {
-            repository.addUser(user)
+            withContext(Dispatchers.Default) {
+                repository.addUser(user)
+            }
         }
+    }
+
+    // expose the private variable -> liveData
+    fun readData(): LiveData<List<User>>{
+        return readAllData
     }
 }
