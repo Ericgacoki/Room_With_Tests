c9c1d41c3194867b8afcee577fbc05f4c9dff5bb
Ericgacoki <gacokieric@gmail.com>
2021-05-01 22:35:43 +0300

    user DAO
    Signed-off-by: Ericgacoki <gacokieric@gmail.com>

--- app/src/main/java/com/ericg/groom/data/UserDao.kt
+++ app/src/main/java/com/ericg/groom/data/UserDao.kt
@@ -10,9 +10,7 @@ import androidx.room.Query
 interface UserDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun addUser(user: User) {
-
-    }
+    suspend fun addUser(user: User)
 
     @Query("SELECT * FROM user_table ORDER BY id ASC")
     fun readAllData():LiveData<List<User>>
