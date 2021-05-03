c650853b0de1ff69f65334748e00b3210aa3b9f2
Ericgacoki <gacokieric@gmail.com>
2021-05-03 03:09:51 +0300

    added @Delete and @Query functions
    Signed-off-by: Ericgacoki <gacokieric@gmail.com>

--- app/src/main/java/com/ericg/groom/data/UserRepository.kt
+++ app/src/main/java/com/ericg/groom/data/UserRepository.kt
@@ -4,7 +4,16 @@ import androidx.lifecycle.LiveData
 
 class UserRepository(private val userDao: UserDao) {
     val readAllData: LiveData<List<User>> = userDao.readAllData()
+
     suspend fun addUser(user: User) {
         userDao.addUser(user)
+    }
+
+    suspend fun deleteUser(user: User){
+        userDao.deleteUser(user)
+    }
+
+    suspend fun deleteAll(){
+        userDao.deleteAll()
     }
 }
