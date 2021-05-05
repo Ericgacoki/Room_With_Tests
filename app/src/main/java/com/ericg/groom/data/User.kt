4aed56d886e3279dde59dc9c42e6fb00eca72290
Ericgacoki <gacokieric@gmail.com>
2021-05-05 12:54:12 +0300

    parcelized User class

--- app/src/main/java/com/ericg/groom/data/User.kt
+++ app/src/main/java/com/ericg/groom/data/User.kt
@@ -1,8 +1,11 @@
 package com.ericg.groom.data
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity(tableName = "user_table")
 data class User(
     @PrimaryKey(autoGenerate = true)
@@ -10,4 +13,4 @@ data class User(
     val firstName:String,
     val lastName: String,
     val age: Int
-)
+): Parcelable
