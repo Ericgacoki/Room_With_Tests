7aa85581261b708569ced9670352f05ed370dc1d
Ericgacoki <gacokieric@gmail.com>
2021-05-05 13:07:39 +0300

    updated DAO

--- app/src/main/java/com/ericg/groom/data/UserDao.kt
+++ app/src/main/java/com/ericg/groom/data/UserDao.kt
@@ -2,6 +2,7 @@ package com.ericg.groom.data
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserDao {
@@ -12,9 +13,15 @@ interface UserDao {
     @Query("SELECT * FROM user_table ORDER BY id ASC")
     fun readAllData(): LiveData<List<User>>
 
+    @Query("DELETE FROM user_table")
+    suspend fun deleteAll()
+
     @Delete
     suspend fun deleteUser(user: User)
 
-    @Query("DELETE FROM user_table")
-    suspend fun deleteAll()
+    @Update
+    suspend fun updateUser(user: User)
+
+    @Query("SELECT * FROM user_table WHERE firstName LIKE :searchQuery OR lastName LIKE :searchQuery")
+    fun searchData(searchQuery: String): LiveData<List<User>>
 }
