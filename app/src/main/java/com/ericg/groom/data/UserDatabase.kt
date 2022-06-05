16e3599ae899d5b4666e54c7e05d07bb9097b6e8
Ericgacoki <gacokieric@gmail.com>
2022-06-05 14:37:57 +0300

    replace manual singleton with hilt di

--- app/src/main/java/com/ericg/groom/data/UserDatabase.kt
+++ app/src/main/java/com/ericg/groom/data/UserDatabase.kt
@@ -1,30 +1,9 @@
 package com.ericg.groom.data
 
-import android.content.Context
-import android.telecom.TelecomManager
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [User::class], version = 1, exportSchema = false)
-abstract class UserDatabase: RoomDatabase() {
-    abstract fun userDao(): UserDao
-
-    companion object{
-
-        @Volatile
-        private var INSTANCE: UserDatabase? = null
-
-        fun getDatabase(context: Context): UserDatabase{
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    UserDatabase::class.java,
-                    "user_database"
-                ).build()
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
+abstract class UserDatabase : RoomDatabase() {
+    abstract val userDao: UserDao
 }
