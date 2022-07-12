48eae202856768eb0083f2a3d33c389cb7fecfe6
Ericgacoki <gacokieric@gmail.com>
2022-07-12 19:41:27 +0300

    increase database version

--- app/src/main/java/com/ericg/groom/data/UserDatabase.kt
+++ app/src/main/java/com/ericg/groom/data/UserDatabase.kt
@@ -3,7 +3,7 @@ package com.ericg.groom.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [User::class], version = 1, exportSchema = false)
+@Database(entities = [User::class], version = 2, exportSchema = false)
 abstract class UserDatabase : RoomDatabase() {
     abstract val userDao: UserDao
 }
